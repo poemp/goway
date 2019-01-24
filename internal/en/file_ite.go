27763250ab4c3be468b3d0ac292c4e731740ef4f
lywworking <xue_2013@sina.com>
2019-01-24 15:19:34 +0800

    修改bug  执行顺序不对

--- internal/en/file_ite.go
+++ internal/en/file_ite.go
@@ -8,4 +8,23 @@ type FileInfo struct {
 	AbsPath string
 	Name    string
 	Code    string
+	Version float64
+}
+
+// 按照 Person.Age 从大到小排序
+type FileInfoSlice []FileInfo
+
+// 重写 Len() 方法
+func (a FileInfoSlice) Len() int {
+	return len(a)
+}
+
+// 重写 Swap() 方法
+func (a FileInfoSlice) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+// 重写 Less() 方法， 从大到小排序
+func (a FileInfoSlice) Less(i, j int) bool {
+	return a[i].Version < a[j].Version
 }
--- internal/file_path.go
+++ internal/file_path.go
@@ -8,6 +8,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -21,30 +23,47 @@ func GetFilePath() []en.FileInfo {
 
 	log.Info("search path:" + absp)
 
-	return getAllPath(absp)
+	e, fils := getAllPath(absp)
+	if e != nil {
+		log.Error(e.Error())
+		return nil
+	}
+	sort.Sort(en.FileInfoSlice(fils))
+	return fils
 }
 
 // this dic all file
-func getAllPath(pathname string) []en.FileInfo {
+func getAllPath(pathname string) (error, []en.FileInfo) {
 	rd, _ := ioutil.ReadDir(pathname)
 	var files []en.FileInfo
 	for _, fi := range rd {
 		if fi.IsDir() {
-			fs := getAllPath(pathname + string(os.PathSeparator) + fi.Name() + string(os.PathSeparator))
+			e, fs := getAllPath(pathname + string(os.PathSeparator) + fi.Name() + string(os.PathSeparator))
+			if e != nil {
+				return e, nil
+			}
 			for _, fe := range fs {
 				files = append(files, fe)
 			}
 		} else {
 			fname := fi.Name()
+			strs := strings.Split(fname, "__")
+			v := strings.Replace(strs[0], "V", "", 1)
+			v = strings.Replace(v, ".", "", 1)
+			f, e := strconv.ParseFloat(v, 64)
+			if e != nil {
+				return e, nil
+			}
 			if strings.HasSuffix(fname, ".sql") {
 				files = append(files, en.FileInfo{
 					File:    fi,
 					AbsPath: pathname + string(os.PathSeparator) + fi.Name(),
 					Name:    fi.Name(),
 					Code:    md.Md5Has(pathname + string(os.PathSeparator) + fi.Name()),
+					Version: f,
 				})
 			}
 		}
 	}
-	return files
+	return nil, files
 }
